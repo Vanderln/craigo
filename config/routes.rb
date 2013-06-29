56bb4cdff5a447a2c6d225fcfc37d99965db55b0
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-29 15:14:31 -0500

    lots more stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Craigo::Application.routes.draw do
-  resources :users
-  resources :posts
+  resources :users do
+    resources :posts
+  end
+
   root :to => 'posts#index'
+
+  get "/login" => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  get "/logout" => 'sessions#destroy'
 end
