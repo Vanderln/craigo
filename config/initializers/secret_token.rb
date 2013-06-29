e003a2873d4d8c7a262494bda03db1e52f37530b
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-28 20:50:10 -0500

    first commit, built user, post model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craigo::Application.config.secret_token = '7b1addf1f3cd1d249e9cae5c27c31af22915ce6b4399c69f9a0de2c1f9b4f4f504471283ef42e840413d3081404c6561865654d74ff5b0d007a51a4f460790a4'
