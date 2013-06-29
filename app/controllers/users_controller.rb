3c52f9d4c67eaee74b08952848195a3532a5cfea
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-29 14:40:33 -0500

    added new, create, show to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,25 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render new_user_path
+    end
   end
 
   def show
+    if current_user
+      @posts = Post.find_all_by_user_id(current_user.id)
+      @comments = Comment.find_all_by_user_id(current_user.id)
+    else
+      redirect :root
+    end
   end
 end
